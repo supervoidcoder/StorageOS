4d9c03100c48c98564243be620db7a9de695387a
MCPro1239953 <94279452+MCPro1239953-Replit@users.noreply.github.com>
2023-07-08 17:35:19 -0500

    Update signin.js
    Redirect to home page on sign in

--- signin.js
+++ signin.js
@@ -3,5 +3,14 @@ const uname = document.getElementById("username");
 const pword = document.getElementById("password");
 
 submit.addEventListener("click", function() {
-  alert(`Username: ${uname.value}\nPassword: ${pword.value}`);
+  //alert(`Username: ${uname.value}\nPassword: ${pword.value}`);
+  const save_pass = prompt("Save username and password?");
+  if (typeof save_pass !=== "null") {
+    // selected 'OK'
+    localStorage.setItem("user", [uname.value, pword.value]);
+  } else {
+    // selected 'Cancel' or closed page or reloaded page
+  }
 });
+
+window.open("https://supervoidcoder.github.io/StorageOS/", "_self")
