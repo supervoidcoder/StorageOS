9ff8b00629e8aefd8c1c758639a87de7302b6168
MCPro1239953 <94279452+MCPro1239953-Replit@users.noreply.github.com>
2023-07-08 17:31:39 -0500

    Update signin.js

--- signin.js
+++ signin.js
@@ -2,10 +2,6 @@ const submit = document.getElementById("submit");
 const uname = document.getElementById("username");
 const pword = document.getElementById("password");
 
-document.addEventListener("load", function() {
-  alert("JavaScript is working!");
-});
-
 submit.addEventListener("click", function() {
   alert(`Username: ${uname.value}\nPassword: ${pword.value}`);
 });
